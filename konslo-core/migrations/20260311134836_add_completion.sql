b480835a71ab743d47de42d247d904b4fea9118f
WhiteBoySurfer <barre.arthur@live.fr>
2026-03-11 16:04:28 +0100

    db: add NOT NULL constraints to completion schema

--- konslo-core/migrations/20260311134836_add_completion.sql
+++ konslo-core/migrations/20260311134836_add_completion.sql
@@ -1,24 +1,22 @@
 -- create an enum
-CREATE TYPE period_enum AS ENUM ('day', 'week', 'month');
+CREATE TYPE goal_period_enum AS ENUM ('day', 'week', 'month');
 
 -- add columns to habits table
-ALTER TABLE habits
-ADD COLUMN goal_value INT,
-ADD COLUMN goal_unit VARCHAR(20),
-ADD COLUMN goal_period period_enum;
+ALTER TABLE habits ADD COLUMN goal_value INT NOT NULL;
+ALTER TABLE habits ADD COLUMN goal_unit VARCHAR(20) NOT NULL;
+ALTER TABLE habits ADD COLUMN goal_period goal_period_enum NOT NULL;
 
 -- add constraints to habits table
-ALTER TABLE habits
-ADD CONSTRAINT habits_goal_value_positive CHECK(goal_value > 0);
+ALTER TABLE habits ADD CONSTRAINT habits_goal_value_positive CHECK(goal_value > 0);
+ALTER TABLE habits ALTER COLUMN created_at SET NOT NULL;
 
 -- create checks table
 CREATE TABLE checks (
     check_id SERIAL PRIMARY KEY,
-    habit_id INT REFERENCES habits(id),
-    value INT,
-    checked_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    habit_id INT REFERENCES habits(id) NOT NULL,
+    value INT NOT NULL,
+    checked_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 -- add constraints to checks
-ALTER TABLE checks
-ADD CONSTRAINT checks_value_positive CHECK(value > 0);
+ALTER TABLE checks ADD CONSTRAINT checks_value_positive CHECK(value > 0);
