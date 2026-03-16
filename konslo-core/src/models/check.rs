00fa3992a2fa9b261df86376fb7572cd06084594
WhiteBoySurfer <barre.arthur@live.fr>
2026-03-16 14:54:35 +0100

    refactor: gate serde derives behind optional feature in konslo-core

--- konslo-core/src/models/check.rs
+++ konslo-core/src/models/check.rs
@@ -1,8 +1,10 @@
-use serde::{Deserialize, Serialize};
-use sqlx::FromRow;
 use time::OffsetDateTime;
 
-#[derive(Debug, FromRow, Serialize)]
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, sqlx::FromRow)]
+#[cfg_attr(feature = "serde", derive(Serialize))]
 pub struct Check {
     pub id: i32,
     pub habit_id: i32,
@@ -10,14 +12,16 @@ pub struct Check {
     pub checked_at: OffsetDateTime,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Deserialize))]
 pub struct CreateCheck {
     pub habit_id: i32,
     pub value: i32,
     pub checked_at: OffsetDateTime,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Deserialize))]
 pub struct UpdateCheck {
     pub id: i32,
     pub value: i32,
--- konslo-core/src/models/habit.rs
+++ konslo-core/src/models/habit.rs
@@ -1,17 +1,20 @@
-use serde::{Deserialize, Serialize};
-use sqlx;
 use time::OffsetDateTime;
 
-#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize, sqlx::Type)]
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Debug, PartialEq, sqlx::Type)]
 #[sqlx(type_name = "goal_period_enum", rename_all = "lowercase")]
-#[serde(rename_all = "lowercase")]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "lowercase"))]
 pub enum GoalPeriod {
     Day,
     Week,
     Month,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Deserialize))]
 pub struct CreateHabit {
     pub name: String,
     pub goal_value: i32,
@@ -19,7 +22,8 @@ pub struct CreateHabit {
     pub goal_period: GoalPeriod,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize, sqlx::FromRow)]
+#[derive(Debug, PartialEq, sqlx::FromRow)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct Habit {
     pub id: i32,
     pub name: String,
