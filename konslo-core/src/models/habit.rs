348dcd5cfcf2409a880695078b06a142a03a0855
WhiteBoySurfer <barre.arthur@live.fr>
2026-03-18 17:33:27 +0100

    feat(core): add HabitWithCheck model

--- konslo-core/src/models/habit.rs
+++ konslo-core/src/models/habit.rs
@@ -1,29 +1,15 @@
 use time::OffsetDateTime;
-
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
+use crate::models::check::Check;
 
 #[derive(Clone, Copy, Debug, PartialEq, sqlx::Type)]
 #[sqlx(type_name = "goal_period_enum", rename_all = "lowercase")]
-#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-#[cfg_attr(feature = "serde", serde(rename_all = "lowercase"))]
 pub enum GoalPeriod {
     Day,
     Week,
     Month,
 }
 
-#[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(Deserialize))]
-pub struct CreateHabit {
-    pub name: String,
-    pub goal_value: i32,
-    pub goal_unit: String,
-    pub goal_period: GoalPeriod,
-}
-
 #[derive(Debug, PartialEq, sqlx::FromRow)]
-#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct Habit {
     pub id: i32,
     pub name: String,
@@ -31,4 +17,23 @@ pub struct Habit {
     pub goal_unit: String,
     pub goal_period: GoalPeriod,
     pub created_at: OffsetDateTime,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct HabitWithCheck {
+    pub id: i32,
+    pub name: String,
+    pub goal_value: i32,
+    pub goal_unit: String,
+    pub goal_period: GoalPeriod,
+    pub created_at: OffsetDateTime,
+    pub checks: Vec<Check>,
+}
+
+#[derive(Debug)]
+pub struct CreateHabit {
+    pub name: String,
+    pub goal_value: i32,
+    pub goal_unit: String,
+    pub goal_period: GoalPeriod,
 }
