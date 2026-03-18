4cce6bf743bd05e08bfa720c7093e5cd3deaadc3
WhiteBoySurfer <barre.arthur@live.fr>
2026-03-18 09:41:16 +0100

    fix(core): derive Debug and PartialEq on Check

--- konslo-core/src/models/check.rs
+++ konslo-core/src/models/check.rs
@@ -1,10 +1,6 @@
 use time::OffsetDateTime;
 
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, sqlx::FromRow)]
-#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[derive(sqlx::FromRow, Debug, PartialEq)]
 pub struct Check {
     pub id: i32,
     pub habit_id: i32,
@@ -12,16 +8,12 @@ pub struct Check {
     pub checked_at: OffsetDateTime,
 }
 
-#[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(Deserialize))]
 pub struct CreateCheck {
     pub habit_id: i32,
     pub value: i32,
     pub checked_at: OffsetDateTime,
 }
 
-#[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(Deserialize))]
 pub struct UpdateCheck {
     pub id: i32,
     pub value: i32,
