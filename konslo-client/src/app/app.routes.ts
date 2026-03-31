fa1fbff6065c9ec131c355d47e64f95f8b70d78e
WhiteBoySurfer <barre.arthur@live.fr>
2026-03-31 12:54:18 +0200

    fix(client): redirect / to /habits

--- konslo-client/src/app/app.routes.ts
+++ konslo-client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HabitListComponent } from './habit/habit-list/habit-list.component';
 import {HabitFormComponent} from "./habit/habit-form/habit-form.component";
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'habits', pathMatch: 'full' },
     { path: 'habits', component: HabitListComponent },
     { path: 'new-habit', component: HabitFormComponent}
 ];
