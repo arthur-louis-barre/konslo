c201ec1765b638ca686e120c38998010ccd8e214
WhiteBoySurfer <barre.arthur@live.fr>
2026-04-01 13:10:47 +0200

    feat(client): implement 3-column grid layout with app shell components

--- konslo-client/src/app/app.component.ts
+++ konslo-client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TopbarComponent} from "./shared/topbar/topbar.component";
+import {LeftSidebarComponent} from "./shared/left-sidebar/left-sidebar.component";
+import {RightSidebarComponent} from "./shared/right-sidebar/right-sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopbarComponent],
+  imports: [RouterOutlet, TopbarComponent, LeftSidebarComponent, RightSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ konslo-client/src/app/shared/left-sidebar/left-sidebar.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+    selector: 'k-left-sidebar',
+    imports: [
+        RouterLink
+    ],
+    templateUrl: './left-sidebar.component.html',
+    styleUrl: './left-sidebar.component.css',
+})
+export class LeftSidebarComponent {
+
+}
