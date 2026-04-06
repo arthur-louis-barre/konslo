159b064884a0108b0ccdb1a4b1e7db17d341df8e
WhiteBoySurfer <barre.arthur@live.fr>
2026-04-06 10:37:45 +0200

    feat(client): disable submit button while request is in flight

--- konslo-client/src/app/habit/habit-form/habit-form.component.ts
+++ konslo-client/src/app/habit/habit-form/habit-form.component.ts
@@ -1,46 +1,52 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import { HabitService } from '../habit.service';
+import {HabitService} from '../habit.service';
 import {GoalPeriod} from "../habit.model";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'k-new-habit-form',
-  imports: [ReactiveFormsModule],
-  templateUrl: './habit-form.component.html',
-  styleUrl: './habit-form.component.css'
+    selector: 'k-new-habit-form',
+    imports: [ReactiveFormsModule],
+    templateUrl: './habit-form.component.html',
+    styleUrl: './habit-form.component.css'
 })
 export class HabitFormComponent {
-  private router = inject(Router);
-  private habitsService = inject(HabitService);
+    private router = inject(Router);
+    private habitsService = inject(HabitService);
 
-  form = new FormGroup({
-    name: new FormControl('', Validators.required),
-    goal_value: new FormControl(1, [Validators.required, Validators.min(1)]),
-    goal_unit: new FormControl('', Validators.required),
-    goal_period: new FormControl('day', Validators.required),
-  });
+    protected isLoading = false;
 
-  onSubmit() {
-    this.habitsService.createHabit({
-      name: this.form.value.name!,
-      goal_value: this.form.value.goal_value!,
-      goal_unit: this.form.value.goal_unit!,
-      goal_period: this.form.value.goal_period as GoalPeriod,
-    }).subscribe(() => {
-      this.router.navigate(['/habits']);
+    form = new FormGroup({
+        name: new FormControl('', Validators.required),
+        goal_value: new FormControl(1, [Validators.required, Validators.min(1)]),
+        goal_unit: new FormControl('', Validators.required),
+        goal_period: new FormControl('day', Validators.required),
     });
-  }
 
-  goBack() {
-    this.router.navigate(['/habits']);
-  }
+    onSubmit() {
+        if (this.isLoading) return;
+        this.isLoading = true;
 
-  get selectedPeriod() {
-    return this.form.controls.goal_period.value;
-  }
+        this.habitsService.createHabit({
+            name: this.form.value.name!,
+            goal_value: this.form.value.goal_value!,
+            goal_unit: this.form.value.goal_unit!,
+            goal_period: this.form.value.goal_period as GoalPeriod,
+        }).subscribe(({
+            next : () => { this.router.navigate(['/habits']); },
+            error: () => { this.isLoading = false },
+        }));
+    }
 
-  setPeriod(period: string) {
-    this.form.controls.goal_period.setValue(period);
-  }
+    goBack() {
+        this.router.navigate(['/habits']);
+    }
+
+    get selectedPeriod() {
+        return this.form.controls.goal_period.value;
+    }
+
+    setPeriod(period: string) {
+        this.form.controls.goal_period.setValue(period);
+    }
 }
